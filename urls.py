035d17141d715e813d6d9e14e22cfd762760aea3
Ernesto Rico Schmidt <e.rico.schmidt@gmail.com>
2008-09-17 17:21:49 -0400

    cleaned up root urls.

--- urls.py
+++ urls.py
@@ -1,27 +1,24 @@
 from django.conf.urls.defaults import *
 from django.contrib import admin
-from django.conf import settings
+from django.contrib.syndication.views import feed
 
-from denklab.articles.feeds import ArticlesFeed, CategoryFeed
-from denklab.presentations.feeds import PresentationsFeed
+from articles.feeds import ArticlesFeed, CategoryFeed
+from presentations.feeds import PresentationsFeed
 
 admin.autodiscover()
 
 feeds = (
-    {'category': CategoryFeed },
+    {'category': CategoryFeed},
     {'articles': ArticlesFeed,
         'presentations': PresentationsFeed}
 )
 
 urlpatterns = patterns('',
     (r'^admin/(.*)', admin.site.root),
+    (r'^contact/', include('contact_form.urls')),
     (r'^articles/', include('denklab.articles.urls')),
     (r'^presentations/', include('denklab.presentations.urls')),
     (r'^projects/', include('denklab.projects.urls')),
-    (r'^contact/', include('contact_form.urls')),
-)
-
-urlpatterns += patterns('django.contrib.syndication.views',
-    (r'^feeds/articles/(?P<url>.*)/$', 'feed', {'feed_dict': feeds[0]}),
-    (r'^feeds/(?P<url>.*)/$', 'feed', {'feed_dict': feeds[1]}),
+    (r'^feeds/articles/(?P<url>.*)/$', feed, {'feed_dict': feeds[0]}),
+    (r'^feeds/(?P<url>.*)/$', feed, {'feed_dict': feeds[1]}),
 )
